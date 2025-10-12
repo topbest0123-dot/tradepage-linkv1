6cdfa203366c94d039d93b9f7ea6544a10ae2504
topbest0123-dot <topbest0123@gmail.com>
2025-10-12 18:16:32 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,11 @@
-{ source: '/:slug', destination: '/_live/:slug' }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: { unoptimized: true },
+  productionBrowserSourceMaps: true, // <— add this
+  async headers() { return []; },
+  async redirects() { return []; },
+  async rewrites() { return []; },
+};
+module.exports = nextConfig;
+// Test rebuild
