465cbd130aa9789ef5d2a7000e8b70f2504df9cb
topbest0123-dot <topbest0123@gmail.com>
2025-10-18 23:14:54 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -16,7 +16,7 @@ export default function HomePage() {
         <div className="container">
           <div className="hero-copy">
             <h1>
-              All your business essential info in one link <br className="hide-d" />
+              Tradespeople: all your info in one link <br className="hide-d" />
              
             </h1>
             <p className="lead">
