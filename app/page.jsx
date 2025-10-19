b5edf06467b5a5066bb2172fd8630c3f2732d850
topbest0123-dot <topbest0123@gmail.com>
2025-10-19 22:43:47 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -16,7 +16,7 @@ export default function HomePage() {
         <div className="container">
           <div className="hero-copy">
             <h1>
-              Tradespeople's online presence done right <br className="hide-d" />
+              Tradespeople's online presence done the right way <br className="hide-d" />
              
             </h1>
             <p className="lead">
